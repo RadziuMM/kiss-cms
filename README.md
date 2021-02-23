2dcefd303eae6c554254df35219e8c483eef1c2a
Radosław Mazurczak <RMazurczak@protonmail.com>
2021-02-23 19:45:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,90 @@
+# KISS-CMS
+
+KISS-CMS is a node.js library for generating cms with MYSQL DB
+
+## Installation
+
+You need a express and mysql library.
+
+```bash
+yarn add kiss-cms express mysql
+```
+or
+```bash
+npm i kiss-cms express mysql
+```
+
+## Usage
+
+### Syntax
+```javascript
+import express from 'express';
+import kisscms from './index.js';
+
+const app = express();
+const port = 3000;
+
+const con = {
+  host: "***",
+  user: "***",
+  password: "***",
+  database:	"***"
+}; 
+
+const cms = [
+  ['table_name',['name','HTML_input_type','DB_cell_type'],['name','HTML_input_type','DB_cell_type'],...etc],
+  ['table_name',['name','HTML_input_type','DB_cell_type'],,...etc],
+  ..etc
+];
+
+kisscms(cms, con, app);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+```
+### Example
+```javascript
+import express from 'express';
+import kisscms from './index.js';
+
+const app = express();
+const port = 8080;
+
+const con = {
+  host: "ShmitDB.amazonaws.com",
+  user: "Shmit123",
+  password: "abc123",
+  database:	"yes"
+}; 
+
+const cms = [
+  ['post',['autor','text','VARCHAR(256)'],['title','text','VARCHAR(256)'],['content','text','VARCHAR(256)'],['date','date','DATE']],
+  ['photo',['photography','file','BLOB'],['title','text','VARCHAR(256)']],
+  ['mail',['addres','text','VARCHAR(256)'],['title','text','VARCHAR(256)'],['content','text','VARCHAR(256)']]
+];
+
+kisscms(cms, con, app);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+```
+## Endpoints
+Wanna fetch some data from db?
+Use this endpoints (ofc u need first provide youre site addres and then add endpoint after '/')
+```bash
+/API                                     - get all records from  all tables 
+/API/table_name                 - get all records from table 
+/API/table_name/number    - get ${number} record from table
+```
+## Usefull information
+1.login/password as default is admin/admin.You can change it directly in db.In futher(maybe tomorrow) i will add GUI to change that in CMS with hash system.
+2.If u looking free mysql DB i recommend JawsDB plugin on heroku.
+3.Yes i will change that ugly css.(if u have idea send me .pdf/.xd/figma)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+GNU GPL
---
+++ export/index.js
@@ -0,0 +1,261 @@
+//generate borderplate
+
+const genborderplate = () =>{
+    document.getElementById('cms').innerHTML=`
+    <div id="loginBox"  name="form">
+        <form action="javascript:login()">
+            <label for="fLogin" class="label">login</label>
+            <input id="login" name="fLogin" class="login" autocomplete="off" required /><br/><br/>
+            <label for="fPass" class="label">password</label>
+            <input id="pass" name="fPass" class="login" type="password" autocomplete="off" required /><br/><br/>
+            <button >Log In</button>
+        </form>
+    </div>
+    <div id="contentBox">
+        <nav>
+        <button onclick="logout()">Log Out</button>
+        </nav>
+        <div id="app">
+        </div>
+    </div>
+    `
+}
+genborderplate();
+
+//get cms
+
+let cms
+fetch('../cms/data')
+    .then(res => res.json())
+    .then(res => {
+        cms = res;
+    }).catch(err => {
+        alert('server no respond!')
+    });
+
+//get data
+
+const getdata = () =>{
+    fetch('../cms/curentData')
+    .then(res => res.json())
+    .then(res => {
+        const names = res[0];
+        res.shift()
+        const table = document.createElement('ul');
+        const dataTable = []
+        let counter = 0
+        res.forEach(array =>{
+            array.forEach(row=>{
+                const x = Object.entries(row)
+                x[0][0]=names[counter]
+                dataTable.push(x)
+            })
+            counter += 1;
+        })
+        dataTable.forEach(element=>{
+            let string = `<from><li id='row${element[0][0]}${element[0][1]}'>`;
+            let counter = 0;
+            element.forEach(entries =>{
+                if(counter === 0){
+                    string+=`<div class="buttons">
+                    <button id="edit${entries[0]}${entries[1]}" onclick='editRow("${entries[0]}" , "${entries[1]}")'>edit</button>
+                    <button onclick='deleteRow("${entries[0]}" , "${entries[1]}")'>remove</button>
+                    </div>
+                    <div class="info" id='info${entries[0]}${entries[1]}'>`
+                }else{
+                    string+=`<div>${entries[0]} : ${entries[1]}</div>`
+                }
+                counter +=1;
+            })
+            string+=`</div></li>
+            <div id='editInfo${element[0][0]}${element[0][1]}' class="editInfo"></div></form>`
+            string+=`<br/><br/>`
+            table.innerHTML += string;
+        })
+        document.getElementById('data').innerHTML += `<h2>All of your entries</h2>`;
+        document.getElementById('data').appendChild(table)
+    }).catch(err => {
+        // alert('server no respond!')
+        console.log(err)
+    });
+}
+
+//  generete html
+
+const gen = () =>{
+    document.getElementById('loginBox').style.display='none';
+    document.getElementById('contentBox').style.display='block';
+    const app =document.getElementById('app');
+    cms.forEach(element => {
+        let cmsTable ='';
+        for(let i =1;i<element.length;i+=1){
+            cmsTable += `
+            <div class='cmsHolder'>
+            <label for="${element[0]}${i-1}" class="label">${element[i][0]}</label>
+            <input id="${element[0]}${i-1}" type="${element[i][1]}" name="${element[0]}${i-1}" autocomplete="off" class="cmsFormInput" required />
+            </div>
+            `
+        }
+        const newElement = document.createElement('div');
+        newElement.className="cmsInput"
+        newElement.innerHTML += `
+        <h3>${element[0]}</h3>
+        <form action="javascript:add(['${element[0]}',${element.length-1}])">
+        ${cmsTable}
+        <button>ADD</button>
+        </form>
+        <br/>
+        `
+        app.appendChild(newElement);
+    });
+    const data = document.createElement('div');
+    data.id= 'data'
+    app.appendChild(data);
+    getdata();
+}
+
+//  html  onclicks
+
+const login = () => {
+    const log=[
+        document.getElementById('login').value,
+        document.getElementById('pass').value
+    ];
+    document.getElementById('login').value='';
+    document.getElementById('pass').value='';
+    const request = new Request('../cms/login', {
+        method: 'POST',
+        body: JSON.stringify(log),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+    fetch(request)
+        .then(res => res.json())
+        .then(res => {
+            if(res ===1){
+                gen();
+            }else{
+                alert('bad login or pass!')
+            }
+        });
+}
+const logout = () => {
+    document.getElementById('loginBox').style.display='block';
+    document.getElementById('contentBox').style.display='none';
+    document.getElementById('app').innerHTML='';
+}
+const add=(arg)=>{
+    const send=[];
+    send.push(arg[0])
+    for(let i =0;i<arg[1];i+=1){
+        send.push(`${document.getElementById(`${arg[0]}${i}`).value}`)
+    }
+    const request = new Request('../cms/insert', {
+        method: 'POST',
+        body: JSON.stringify(send),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+    fetch(request)
+        .then(res => res.json())
+        .then(res => {
+            console.log('New insert')
+        });
+}
+let isEditing = false;
+const editRow = (arg0,arg1) =>{
+    if(isEditing){
+        //true
+        let button = document.getElementsByTagName('button');
+        for(let i = 0;i<button.length;i+=1){
+            button[i].disabled = false;
+        }
+        document.getElementById(`edit${arg0}${arg1}`).innerHTML = 'edit';
+        document.getElementById(`info${arg0}${arg1}`).style.display = 'block';
+        document.getElementById(`editInfo${arg0}${arg1}`).style.display = 'none';
+
+        let counter = 0;
+        let stopAt;
+        cms.forEach(element =>{
+            if(element[0] === arg0){
+                stopAt = counter;
+            }
+            counter += 1
+        })
+        let counter0 = 0;
+        const sendData = [];
+        cms[stopAt].forEach(element=>{
+            if(counter0 === 0){}else{
+                sendData.push(document.getElementById(`edited${element[0]}`).value);
+            }
+            counter0 += 1;
+        })
+        document.getElementById(`editInfo${arg0}${arg1}`).innerHTML = '';
+        const request = new Request('../cms/edit', {
+            method: 'POST',
+            body: JSON.stringify([[arg0,arg1],sendData]),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        });
+        fetch(request)
+            .then(res => res.json())
+            .then(res => {
+                console.log('One row has been edited')
+            });
+
+
+        isEditing = false
+    }else{
+        //false
+        let button = document.getElementsByTagName('button');
+        for(let i = 0;i<button.length;i+=1){
+            button[i].disabled = true;
+        }
+        document.getElementById(`edit${arg0}${arg1}`).disabled = false;
+        document.getElementById(`edit${arg0}${arg1}`).innerHTML = 'save!';
+        document.getElementById(`info${arg0}${arg1}`).style.display = 'none';
+        let counter = 0;
+        let stopAt;
+        cms.forEach(element =>{
+            if(element[0] === arg0){
+                stopAt = counter;
+            }
+            counter += 1
+        })
+        const editInfo = document.getElementById(`editInfo${arg0}${arg1}`);
+        editInfo.style.display = 'block';
+        let string = '';
+        let counter0 = 0;
+        cms[stopAt].forEach(element=>{
+            if(counter0 === 0){}else{
+                string += `
+                <div class="edited">
+                    <label for="${element[0]}" class="label">${element[0]}</label>
+                    <input id="edited${element[0]}" name="edited${element[0]}" type="${element[1]}"  autocomplete="off" required />
+                </div>`
+            }
+             counter0 += 1;
+        })
+        editInfo.innerHTML = string;
+
+        isEditing = true
+    }
+}
+const deleteRow = (arg0,arg1) =>{
+    const send=[arg0,arg1];
+    const request = new Request('../cms/remove', {
+        method: 'POST',
+        body: JSON.stringify(send),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+    fetch(request)
+        .then(res => res.json())
+        .then(res => {
+            console.log('One row has been removed')
+        });
+}
